251b236ad7aa272dfc2ad78515a75d8b758b7445
syumbel <syu.kashapova@mail.ru>
2022-11-04 22:23:57 +0500

    добавила задания

---
+++ hw01/Program.cs
@@ -0,0 +1,16 @@
+﻿/*  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+int []array =new int [new Random().Next(3 ,3)];
+for(int i=0;i<array.Length;i++)
+{
+    array[i]=new Random().Next(100,999);
+      Console.WriteLine($"{array[i]}");
+Console.WriteLine();
+}
+int A=0;
+foreach(var item in array)
+   if (item%2==0)
+   A++;
+
+Console.Write(A);
---
+++ hw02/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+
+*/
+int []array =new int [new Random().Next(4 ,4)];
+int sum=0;
+int sum1=0;
+for(int i=0;i<array.Length;i++)
+{
+    array[i]=new Random().Next(-6,89);
+      Console.WriteLine($"{array[i]}");
+Console.WriteLine();
+if (i%2==0)
+sum=sum+array[i];
+else 
+sum1=sum1+array[i];
+
+}
+
+
+Console.Write("сумму элементов, стоящих на нечётных позициях " + sum1);
